97884b87b47ae2361ebb0e7ff8eb6bda18bb191b
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2024-05-07 21:21:42 -0600

    fix: issues in database, feat: added total products availables in dashboard page

--- prisma/migrations/20240506022829_remove_date_in_suministro/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `fecha` on the `suministro` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `suministro` DROP COLUMN `fecha`;
