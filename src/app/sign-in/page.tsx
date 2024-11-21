1b3f73fa80805c466f08170e06dd30f16379fd5b
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2024-11-21 09:59:10 -0600

    fixed issues in authentication method

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -10,6 +10,7 @@ export default function SignInForm() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const router = useRouter();
+  const inputPasswordRef = React.useRef<HTMLInputElement>(null);
  
   // Handle the submission of the sign-in form
   const handleSubmit = async (e: React.FormEvent) => {
@@ -43,6 +44,12 @@ export default function SignInForm() {
       console.error(JSON.stringify(err, null, 2));
     }
   };
+
+  const showPassword = () => {
+    if (inputPasswordRef.current) {
+      inputPasswordRef.current.type = inputPasswordRef.current.type === 'password' ? 'text' : 'password';
+    }
+  }
  
   // Display a form to capture the user's email and password
   return (
@@ -69,17 +76,26 @@ export default function SignInForm() {
                 value={email}
             />
           </div>
-          <div>
+          <div className="flex items-stretch border border-gray-400 rounded-xl bg-white overflow-hidden pr-2">
             <input
-                className="text-black border border-gray-400 w-full p-2 rounded-xl"
-                placeholder="**********"
+                className="text-black w-full p-2 border-none outline-transparent"
+                placeholder="your password"
                 onChange={(e) => setPassword(e.target.value)}
                 id="password"
                 name="password"
+                ref={inputPasswordRef}
                 type="password"
                 value={password} />
+            <button type="button" onClick={showPassword}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path
+                    d="M12 9C11.2044 9 10.4413 9.31607 9.87868 9.87868C9.31607 10.4413 9 11.2044 9 12C9 12.7956 9.31607 13.5587 9.87868 14.1213C10.4413 14.6839 11.2044 15 12 15C12.7956 15 13.5587 14.6839 14.1213 14.1213C14.6839 13.5587 15 12.7956 15 12C15 11.2044 14.6839 10.4413 14.1213 9.87868C13.5587 9.31607 12.7956 9 12 9ZM12 17C10.6739 17 9.40215 16.4732 8.46447 15.5355C7.52678 14.5979 7 13.3261 7 12C7 10.6739 7.52678 9.40215 8.46447 8.46447C9.40215 7.52678 10.6739 7 12 7C13.3261 7 14.5979 7.52678 15.5355 8.46447C16.4732 9.40215 17 10.6739 17 12C17 13.3261 16.4732 14.5979 15.5355 15.5355C14.5979 16.4732 13.3261 17 12 17ZM12 4.5C7 4.5 2.73 7.61 1 12C2.73 16.39 7 19.5 12 19.5C17 19.5 21.27 16.39 23 12C21.27 7.61 17 4.5 12 4.5Z"
+                    fill="black"/>
+              </svg>
+            </button>
           </div>
-          <button type="submit" className="bg-primary w-full text-white rounded-md py-2 hover:bg-primary/30">Sign In</button>
+          <button type="submit" className="bg-primary w-full text-white rounded-md py-2 hover:bg-primary/30">Sign In
+          </button>
         </div>
       </form>
     </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,10 @@ import { currentUser } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up', '/home/dashboard']);
 const isAdminRoute = createRouteMatcher(['/home/admin(.*)']);
 
-export default clerkMiddleware(async (_auth, request) => {
-  const auth = _auth()
+export default clerkMiddleware(async (auth, request) => {
 
   if(!isPublicRoute(request)) {
-    auth.protect();
+    await auth.protect();
   }
 
 }, { debug: process.env.NODE_ENV === 'development' });
