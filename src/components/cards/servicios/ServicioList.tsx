03c004daba6690714e437d3bf3a295763c7cf1e4
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2024-12-01 03:21:32 -0600

    responsive design serviciosList

--- src/components/cards/servicios/ServicioList.tsx
+++ src/components/cards/servicios/ServicioList.tsx
@@ -39,7 +39,7 @@ export default function ServicioList(cliente: { cliente: ClienteServiceById }) {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-4">
         { servicios.length === 0 && Array(100).fill(' ').map((_, key) => (
             <ClientItemSkeleton key={key} />
         ))}
