d6c5cadbb15350d11f0826ed08dec9bae2789b68
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2024-12-02 03:26:48 -0600

    fixed conflicts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,9 @@ import { currentUser } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in",
   "/sign-up",
-<<<<<<< HEAD
   "/api/services",
   "/home/dashboard",
-=======
   "/home/presentation",
->>>>>>> d2dfb418f0d68a81b021daf46089af8997b18c56
 ]);
 const isAdminRoute = createRouteMatcher(["/home/admin(.*)"]);
 
