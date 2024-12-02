b0a7146533d765d0ebe530f2ce3909df5abe5f8e
Egex563 <erickespinoza349@gmail.com>
2024-12-02 13:11:45 -0600

    mensaje redireccion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
       <main className="min-h-screen md:grid justify-center items-center">
         <section>
-          <p className="font-bold text-3xl">Sitio equivocado, ir a <Link className="text-slate-600 inline-flex items-center" href="/home/presentation">Acerca de <OpenIcon width={28} height={28} className="fill-gray-400" /></Link></p>
+          <p className="font-bold text-3xl">Volver al modulo  <Link className="text-slate-600 inline-flex items-center" href="/home/presentation">Acerca de <OpenIcon width={28} height={28} className="fill-gray-400" /></Link></p>
         </section>
       </main>
   );
